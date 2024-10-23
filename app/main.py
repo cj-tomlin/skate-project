a927e4984d5e6c5c6e8e48cefb546eb1d25bd746
Brad McNally <BradMcNally@hotmail.com>
2024-10-23 17:57:43 +0100

    added config and some stuff for fastapi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
+import os
+import webbrowser
+
+import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
+import config
+
 from fastapi import FastAPI
 from app.api import routes
 
-app = FastAPI()
+###################################################################
+## Initialise FastAPI session
+###################################################################
 
-app.include_router(routes.router)
+app = FastAPI(title="Skate Backend", version="0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+if __name__ == "__main__":
+    print(f"CWD = {os.getcwd()}")
+    webbrowser.open(f"http://{config.HOSTNAME}:{config.PORT}/docs")
+    uvicorn.run("main:app", host=config.HOSTNAME, port=config.PORT)
