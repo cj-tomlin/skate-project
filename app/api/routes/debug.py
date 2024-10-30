3ed00a2c216e9f27623c2b1ba5c5c826d3ec9804
Chris Tomlin <tomlinc98@gmail.com>
2024-10-30 15:39:08 +0000

    chore: re-organise route handling

--- app/api/routes/debug.py
+++ app/api/routes/debug.py
@@ -6,8 +6,3 @@ router = APIRouter()
 @router.get("/")
 async def root():
     return {"message": "Application running"}
-
-
-@router.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
